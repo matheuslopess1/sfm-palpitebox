918efec36737fcea7cc61b62bd7bb3941bdbef51
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-03 11:43:11 -0300

    Encapsula tudo e cria uma barra de menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,16 +3,19 @@ import styles from "./styles.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className="container mx-auto">
-        <img
-          className="mx-auto"
-          src="/logo_palpitebox.png"
-          alt="PalpiteBox"
-          style={{ maxWidth: "144px" }}
-        />
+    <React.Fragment>
+      <div className={styles.wrapper}>
+        <div className="container mx-auto">
+          <img
+            className="mx-auto"
+            src="/logo_palpitebox.png"
+            alt="PalpiteBox"
+            style={{ maxWidth: "144px" }}
+          />
+        </div>
       </div>
-    </div>
+      <div className="bg-gray-300 p-4 shadow-md">sobre</div>
+    </React.Fragment>
   );
 };
 
