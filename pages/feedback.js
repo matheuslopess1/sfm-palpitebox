34707298b00b73764013064f668a53209116b27e
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-03 10:45:17 -0300

    Adiciona link para página inicial

--- pages/feedback.js
+++ pages/feedback.js
@@ -2,7 +2,16 @@ import React from "react";
 import Link from "next/link";
 
 const Feedback = () => {
-  return <h1>Pesquisa</h1>;
+  return (
+    <div>
+      <h1>Pesquisa</h1>
+      <div>
+        <Link href="/">
+          <a>Página Inicial</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Feedback;
