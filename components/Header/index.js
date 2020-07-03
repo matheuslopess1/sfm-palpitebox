1b4732663309ce21a0fbce5d8da362266fe7bd21
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-03 11:49:53 -0300

    Adiciona e estiliza as opções do menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -14,7 +15,20 @@ const Header = () => {
           />
         </div>
       </div>
-      <div className="bg-gray-300 p-4 shadow-md">sobre</div>
+      <div className="bg-gray-300 p-4 shadow-md text-center">
+        <Link href="/">
+          <a className="px-2 hover:underline">Página Inicial</a>
+        </Link>
+        <Link href="/about">
+          <a className="px-2 hover:underline">Sobre</a>
+        </Link>
+        <Link href="/contact">
+          <a className="px-2 hover:underline">Contato</a>
+        </Link>
+        <Link href="/feedback">
+          <a className="px-2 hover:underline">Pesquisa</a>
+        </Link>
+      </div>
     </React.Fragment>
   );
 };
