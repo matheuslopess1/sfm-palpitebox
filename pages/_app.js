f9aebd5544c7ae58906ac2dac2496cf9ab44ded0
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-03 12:44:25 -0300

    Remove importações desnecessárias

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import "../css/styles.css";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 
 const App = ({ Component, pageProps }) => {
