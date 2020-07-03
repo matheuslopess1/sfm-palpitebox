1e580b667a522f2380aa0fe2ac9bcc7cc14a2db6
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-03 11:38:50 -0300

    Adiciona a logomarca do palpitebox

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,7 +4,14 @@ import styles from "./styles.module.css";
 const Header = () => {
   return (
     <div className={styles.wrapper}>
-      <div className="container mx-auto">PalpiteBox</div>
+      <div className="container mx-auto">
+        <img
+          className="mx-auto"
+          src="/logo_palpitebox.png"
+          alt="PalpiteBox"
+          style={{ maxWidth: "144px" }}
+        />
+      </div>
     </div>
   );
 };
