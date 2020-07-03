9cdea0936bc134a610c04210d3109c75b1aee39a
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-03 11:19:17 -0300

    Desacopla e cria um componente Header

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "../css/styles.css";
 
+import Header from "../components/Header";
+
 const App = ({ Component, pageProps }) => {
   return (
     <div>
-      <h1>App</h1>
+      <Header />
       <Component {...pageProps} />
     </div>
   );
