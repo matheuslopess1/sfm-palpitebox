f8a5b3a204580e8239c9e622f5c56fedd7afd26b
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-03 12:52:52 -0300

    Troca para o conteúdo real da página inicial

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,21 @@ import Link from "next/link";
 const Index = () => {
   return (
     <div>
-      <h1>Olá Semana Fullstack Master!</h1>
-      <div>
-        <Link href="/about">
-          <a>Sobre</a>
-        </Link>
-        <Link href="/contact">
-          <a>Contato</a>
-        </Link>
+      <p className="mt-12 text-center">
+        O restaurante X sempre busca por atender melhor seus clientes.
+        <br />
+        Por isso, estamos sempre abertos a ouvir a sua opinião.
+      </p>
+      <div className="text-center my-12">
         <Link href="/feedback">
-          <a>Pesquisa</a>
+          <a className="bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow">
+            Dar opinião ou sugestão
+          </a>
         </Link>
       </div>
+      <p className="my-12 text-center">
+        Ao dar sua opinião e/ou sugestão, ganhe 10% na sua próxima compra.
+      </p>
     </div>
   );
 };
