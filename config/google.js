2989c2914293c07a0c8f3b858fca77047e8c2b2a
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-06 14:17:10 -0300

    Buffer agora nao exibe mensagem de depreciacao

--- config/google.js
+++ config/google.js
@@ -1,4 +1,4 @@
-const fromBase64 = (value) => new Buffer(value, "base64").toString("ascii");
+const fromBase64 = (value) => Buffer.from(value, "base64").toString("ascii");
 
 export const credentials = {
   private_key: fromBase64(process.env.GOOGLE_CREDENTIALS_PRIVATE_KEY),
