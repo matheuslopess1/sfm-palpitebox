aa9b857f1f075b4659cd2f4289d34ace30064e5f
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-07-06 13:54:33 -0300

    Organiza as credenciais do Google

---
+++ config/google.js
@@ -0,0 +1,6 @@
+export const credentials = {
+  private_key: process.env.GOOGLE_CREDENTIALS_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_CREDENTIALS_CLIENT_EMAIL,
+};
+
+export const sheetId = process.env.GOOGLE_CREDENTIALS_SHEET_ID;
